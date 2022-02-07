a6cce68c316af983773b808642d2f8f5ef7a4894
Frank <fraco.1918@gmail.com>
2022-02-07 15:06:03 -0500

    admin & diplo views

--- src/views/Administrative_view.jsx
+++ src/views/Administrative_view.jsx
@@ -4,9 +4,12 @@ import '../css/Administrative_view.css'
 export default function Administrative_view() {
     return <div className='admin_view'>
         <div className="tabs">
-            <div className="next_events">next event</div>
-            <div className="relations">relations</div>
-            <div className="laws">laws</div>
+            <div className="taxes">taxes</div>
+            <div className="army_maintenance">maintenance</div>
+            <div className="production">production</div>
+            <div className="imports">imports</div>
+            <div className="dummy"></div>
+            <div className="exports">exports</div>
         </div>
         <div className="work_area">
             work area
--- src/views/diplomatic_view.jsx
+++ src/views/diplomatic_view.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import '../css/Diplomatic_view.css'
 
 export default function Diplomatic_view() {
-    return <div className='diplomatic_view'>
-
+    return <div className='diplo_view'>
+        <div className="tabs">
+            <div className="next_events">next event</div>
+            <div className="relations">relations</div>
+            <div className="laws">laws</div>
+        </div>
+        <div className="work_area">
+            work area
+        </div>
     </div>;
 }
