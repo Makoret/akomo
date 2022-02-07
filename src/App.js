8d90c359ea8c4a5530a23275241c18f290f9f00f
Frank <fraco.1918@gmail.com>
2022-02-07 11:09:59 -0500

    menu

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './css/App.css';
+import Menu from './views/Menu.jsx';
+
+function App() {
+  return (
+    <Menu />
+  );
+}
+
+export default App;
---
+++ src/views/Menu.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../css/Menu.css';
+
+export default function menu() {
+    return (
+        <div className="intro">
+            <div className="title">
+                <h1>akomo</h1>
+            </div>
+            <div className="menu">
+                <div className="menu_startGame">start game</div>
+                <div className="menu_loadGame">load game</div>
+                <div className="menu_options">options</div>
+                <div className="menu_credits">credits</div>
+                <span className='version'>1.0.0</span>
+            </div>
+        </div>
+    )
+}
